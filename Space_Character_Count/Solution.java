edd8e6c1df1780a0c8cddec7909420c61e4dbf81
rajsinghchandel <104884116+rajsinghchandel@users.noreply.github.com>
2023-02-24 18:57:14 +0530

    Update Solution.java

--- Space_Character_Count/Solution.java
+++ Space_Character_Count/Solution.java
@@ -4,14 +4,14 @@ public class MyClass {
     public static void main(String args[]) {
      Scanner srt =new Scanner (System.in);
      String str= srt.nextLine();
-     String s= str.toLowerCase();
+     String s= str.toLowerCase(); //to convert into lower case
      int count =0;
      int count1 =0;
      for(int i=0;i<s.length();i++){
-         if(s.charAt(i)==' '){
+         if(s.charAt(i)==' '){     //to compare with equal
              count++;
          }
-         if(s.charAt(i)>'a' && s.charAt(i)<'z'){
+         if(s.charAt(i)>'a' && s.charAt(i)<'z'){    //to check character from a to z
              count1++;
          }
      }
